36a32ca09c9298beee5d50bf694dccdfb8882f93
ywangnon <ywangnon123@gmail.com>
2020-03-29 23:27:12 +0900

    add project

---
+++ SwiftUI_Sample/SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Sample
+//
+//  Created by Hansub Yoo on 2020/03/29.
+//  Copyright © 2020 Hansub Yoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
